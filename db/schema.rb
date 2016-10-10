9ca08f861e67058edf703caf1ae6a51c5ac41242
Yorke000 <hieda.yutaro@gmail.com>
2016-10-10 11:40:03 +0000

    user-profile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161001033755) do
+ActiveRecord::Schema.define(version: 20161010055409) do
 
   create_table "microposts", force: :cascade do |t|
     t.integer  "user_id"
@@ -25,7 +25,9 @@ ActiveRecord::Schema.define(version: 20161001033755) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
+    t.string   "profile"
     t.string   "email"
+    t.string   "area"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
