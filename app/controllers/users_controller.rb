fe433339d38a8146d3b38bf2c374cb81423c1c5c
Yorke000 <hieda.yutaro@gmail.com>
2016-10-10 12:34:28 +0000

    shuusei

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :me?, only: [:edit, :update]
+
   
   def show
     @user = User.find(params[:id])
@@ -33,6 +36,19 @@ class UsersController < ApplicationController
     end
   end
   
+  def me?
+    @user = User.find(params[:id])
+    if current_user != @user
+      redirect_to root_path
+    end
+  end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end  
 
   private
 
